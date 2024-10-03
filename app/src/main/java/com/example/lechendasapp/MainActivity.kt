f2ea1e77a035f63f0bc457f255e3b485a9e7dd50
Daniel-gmol <117100281+Daniel-gmol@users.noreply.github.com>
2024-10-03 15:57:18 -0600

    Main act

--- app/src/main/java/com/example/lechendasapp/MainActivity.kt
+++ app/src/main/java/com/example/lechendasapp/MainActivity.kt
@@ -4,14 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.lechendasapp.Views.NewPasswordScreen
+import androidx.compose.ui.res.stringResource
+import com.example.lechendasapp.navigator.AppNavigator
+import com.example.lechendasapp.views.IntroImage
 import com.example.lechendasapp.ui.theme.LechendasAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +15,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             LechendasAppTheme {
-                NewPasswordScreen()
-                }
+                AppNavigator()
             }
         }
     }
-
-
+}
