9b8619bef86813745978830b135f4f950f8bf13c
Daniel-gmol <117100281+Daniel-gmol@users.noreply.github.com>
2024-11-27 08:32:44 -0600

    fixes de tests

--- app/src/androidTest/java/com/example/lechendasapp/SearchScreenTest.kt
+++ app/src/androidTest/java/com/example/lechendasapp/SearchScreenTest.kt
@@ -1,19 +1,12 @@
 package com.example.lechendasapp
 
-import android.app.Application
-import androidx.compose.ui.test.*
-import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.foundation.layout.*
 import androidx.compose.ui.test.assertIsDisplayed
-import androidx.compose.ui.test.onNodeWithContentDescription
-import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
-import androidx.compose.ui.test.performTextInput
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.lechendasapp.data.model.MonitorLog
 import com.example.lechendasapp.screens.SearchContent
-import com.example.lechendasapp.screens.SearchScreen
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -27,10 +20,8 @@ class SearchScreenTest {
 
     @Test
     fun testLogList() {
-        // Mock data for the log list
         val mockLogList = List(10) { MonitorLog(id = it.toLong(), logType = "Type $it", dateMillis = 123456789, gpsCoordinates = "12.34, 56.78", climateType = "Tropical", seasons = "Summer", zone = "Zone 1", userId = "123", location = "") }
 
-        // Set the content of the screen
         composeTestRule.setContent {
             SearchContent(
                 logList = mockLogList,
